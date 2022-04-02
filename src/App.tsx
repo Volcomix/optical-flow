cd62dfea3438d6ffb8ef6545c004a573d4bf36fe
Volcomix <Volcomix@users.noreply.github.com>
2022-04-02 18:27:26 +0200

    Fix default video URL in GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
-import sintel from './assets/videos/sintel-2048-surround.mp4'
+
+// Makes file handled by git lfs work in GitHub Pages
+const sintelUrl =
+  'https://media.githubusercontent.com/media/volcomix/optical-flow/main/src/assets/videos/sintel-2048-surround.mp4'
 
 function App() {
   return (
     <div className="App">
-      <video className="App-videoPreview" src={sintel} controls />
+      <video className="App-videoPreview" src={sintelUrl} controls />
     </div>
   )
 }
