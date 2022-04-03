11734521aa20eab89da6088f5d86498523fd396c
Volcomix <Volcomix@users.noreply.github.com>
2022-04-03 20:16:22 +0200

    Update test reporters

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,7 @@ const config: PlaywrightTestConfig = {
   workers: process.env.CI ? 4 : undefined,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['list'], ['html', { open: 'never' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
