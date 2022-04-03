c0237465b2656478d7e84e0f0d0cb66321ee8a8f
Volcomix <Volcomix@users.noreply.github.com>
2022-04-03 16:51:22 +0200

    Remove webkit tests from CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ const config: PlaywrightTestConfig = {
   // retries: process.env.CI ? 2 : 0,
 
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 3 : undefined,
+  workers: process.env.CI ? 2 : undefined,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
@@ -59,16 +59,16 @@ const config: PlaywrightTestConfig = {
       },
     },
 
-    ...(process.env.CI
-      ? [
-          {
-            name: 'webkit',
-            use: {
-              ...devices['Desktop Safari'],
-            },
-          },
-        ]
-      : []),
+    // ...(process.env.CI
+    //   ? [
+    //       {
+    //         name: 'webkit',
+    //         use: {
+    //           ...devices['Desktop Safari'],
+    //         },
+    //       },
+    //     ]
+    //   : []),
 
     /* Test against mobile viewports. */
     // {
