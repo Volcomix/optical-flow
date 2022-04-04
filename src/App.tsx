c3a8f2ffd8bccdce3cf819aebedeb94f4e17f159
Volcomix <Volcomix@users.noreply.github.com>
2022-04-04 19:55:48 +0200

    Use CSS modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import './App.css'
+import classes from './App.module.css'
 
 const videosUrl =
   'https://github.com/volcomix/optical-flow/releases/download/assets/videos/v1.0.0'
 
 function App() {
   return (
-    <div className="App">
-      <ul className="App-inputVideos" aria-label="Input videos">
+    <div className={classes.root}>
+      <ul className={classes.inputVideos} aria-label="Input videos">
         <li>Sintel</li>
       </ul>
       <video
-        className="App-videoPreview"
+        className={classes.inputVideoPreview}
         aria-label="Input video preview"
         src={`${videosUrl}/sintel-2048-surround.mp4`}
         controls
